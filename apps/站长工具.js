bce539a0bfc70d8c42005040ac32ff1c926c7d6b
饼干 <2562248115@qq.com>
2023-08-26 09:12:11 +0000

    update apps/站长工具.js.
    Signed-off-by: 饼干 <2562248115@qq.com>

--- apps/站长工具.js
+++ apps/站长工具.js
@@ -1,4 +1,8 @@
+import { segment } from "oicq";
+import plugin from '../../../lib/plugins/plugin.js';
+import axios from 'axios';
 import fetch from 'node-fetch';
+import lodash from 'lodash';
 
 export class CombinedPlugin extends plugin {
   constructor() {
@@ -50,21 +54,19 @@ export class CombinedPlugin extends plugin {
     }
   }
 
-   async function handleQueryWeather(e) {
+   async handleQueryWeather(e) {
     try {
-        const match = e.msg.match(/#查天气(.+)/);
-        const area = match ? match[1] : "";
-        const imgURL = `https://api.lolimi.cn/api/qqtq/t?msg=${area}&key=BM9omb4w3kTNrpopk7ZmkokSMj`;
-        const msg = [segment.image(imgURL)];
-        
-        await this.reply(msg);
-        return true;
+    const match = e.msg.match(/#查天气(.+)/);
+    const area = match ? match[1] : "";
+    let img_1 = `https://api.lolimi.cn/api/qqtq/t?msg=${area}&key=BM9omb4w3kTNrpopk7ZmkokSMj`;
+    let msg = [segment.image(`${img_1}`)];
+      await this.reply(msg);
+      return true;
     } catch (error) {
-        await e.reply('接口请求错误');
-        return false;
+      await e.reply('接口请求错误');
+      return false;
     }
 }
-
   async handlePingUrl(e) {
     const url = e.msg.match(/^#ping (.+)$/)[1];
     const apiKey = 'IBVWYwYGKEgdG4dErUL1SwXdmO';
