1f052d179bf3795e6078fd52547b9c3903ac3955
饼干 <2562248115@qq.com>
2023-08-26 03:26:26 +0000

    生活在白狐群里的饼干
    Signed-off-by: 饼干 <2562248115@qq.com>

---
+++ apps/查IP.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+import { plugin } from '../../lib/plugins/plugin.js';
+
+export class ExamplePlugin extends plugin {
+  constructor() {
+    super({
+      name: '查询IP信息',
+      dsc: '查询IP信息',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: '^#查(IP|ip)(.*)$',
+          fnc: 'ymcx'
+        }
+      ]
+    });
+  }
+
+  async ymcx(e) {
+    const str = e.msg.match(/#查IP(.+)/);
+    const regex = /\d+\.\d+\.\d+\.\d+/;
+    const match = regex.exec(str[1]);
+    const ip = match[0];
+    const url = `https://api.vvhan.com/api/getIpInfo?ip=${ip}`;
+    const response = await fetch(url).catch((err) => logger.error(err));
+    if (!response) {
+      return await this.reply('接口请求失败');
+    }
+    const res = await response.json();
+    const res2 = res.info;
+    const msg = [
+      "IP：", res.ip, "\n",
+      "国：", res2.country, "\n",
+      "省：", res2.prov, "\n",
+      "城：", res2.city, "\n",
+      "运营商：", res2.lsp
+    ];
+    await e.reply(msg);
+  }
+}
---
+++ apps/查天气.js
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch';
+import { plugin } from '../../lib/plugins/plugin.js';
+
+export class ExamplePlugin extends plugin {
+  constructor() {
+    super({
+      name: '查询天气信息',
+      dsc: '查询天气信息',
+      event: 'message',
+      priority: 1,
+      rule: [
+        {
+          reg: '^#查天气(.*)$',
+          fnc: 'tqcx'
+        }
+      ]
+    });
+  }
+
+  async tqcx(e) {
+    const region = e.msg.match(/^#查天气(.+)$/)[1];
+    const url = `https://api.vvhan.com/api/weather?city=${region}`;
+    const response = await fetch(url).catch((err) => logger.error(err));
+    if (!response) {
+      return await this.reply('接口请求失败');
+    }
+    const res = await response.json();
+    const error = res.success;
+    if (error !== true) {
+      await e.reply('查询出错');
+      return;
+    }
+    const msg = [
+      "城市：", res.city, "\n",
+      "时间：", res.info.date, "\n",
+      "天气状态：", res.info.type, "\n",
+      "最高温：", res.info.high, "\n",
+      "最低温：", res.info.low, "\n",
+      "风向：", res.info.fengxiang, "\n",
+      "风级：", res.info.fengli, "\n",
+      "tip：", res.info.tip
+    ];
+    await e.reply(msg);
+  }
+}
