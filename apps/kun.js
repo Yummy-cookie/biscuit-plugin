abbd7edd274ac84693d8458db8369affeae022f2
饼干 <2562248115@qq.com>
2023-08-26 11:11:31 +0000

    生活在白狐群里的饼干
    Signed-off-by: 饼干 <2562248115@qq.com>

---
+++ apps/kun.js
@@ -0,0 +1,29 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import {segment} from 'oicq'
+
+
+export class example extends plugin {
+	constructor() {
+		super({
+			/** 功能名称 */
+			name: '原神KFC语音',
+			/** 功能描述 */
+			dsc: '简单开发示例',
+			/** https://oicqjs.github.io/oicq/#events */
+			event: 'message',
+			/** 优先级，数字越小等级越高 */
+			priority: 5000,
+			rule: [{
+					/** 命令正则匹配 */
+					reg: '^#?kun音乐.*$',
+					/** 执行方法 */
+					fnc: 'kunawa'
+				}
+			]
+		})
+	}
+
+	async kunawa(e) {
+		e.reply(segment.record('https://api.lolimi.cn/api/kunkun/k?key=IBVWYwYGKEgdG4dErUL1SwXdmO'))
+	}
+}
