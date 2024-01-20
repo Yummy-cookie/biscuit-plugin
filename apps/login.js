b255812443d71fefdda80889977016fbdb9b7287
饼干 <2562248115@qq.com>
2024-01-20 12:46:52 +0000

    修复bug
    Signed-off-by: 饼干 <2562248115@qq.com>

--- apps/login.js
+++ apps/login.js
@@ -1,20 +1,20 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'stmp.server.com',
-  port: 587,
-  secure: false,
+  host: 'your-smtp-host', // stmp服务器
+  port: 587, // stmp端口
+  secure: false, // false是tls,true是ssl
   auth: {
-    user: '发送邮件用户',
-    pass: '密码',
+    user: 'your-email@example.com', // stmp账号
+    pass: 'your-email-password', // stmp账号的密码
   },
 });
 
-const emailRecipient = '填写自己的邮箱';
+const emailRecipient = 'your-email@example.com'; // 接收人的邮箱
 
 function sendNotificationEmail() {
   const mailOptions = {
-    from: '发送邮件用户',
+    from: 'your-email@example.com', // 发送的邮件账户
     to: emailRecipient,
     subject: 'QQ机器人已掉线提醒',
     text: 'QQ机器人已掉线，请检查并重新运行。',
