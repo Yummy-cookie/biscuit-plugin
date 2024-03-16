4055dea477566d6fb41d97bf0b1f9cc8672fcda9
饼干 <2562248115@qq.com>
2024-03-16 10:42:19 +0800

    fix✅

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Common from './Common.js'
 import Cfg from './Cfg.js'
 import Version from './Version.js'
 import App from './App.js'
+import MiaoError from './MiaoError.js'
 
-export { Data, Cfg, Common, Version, App }
+export { Data, Cfg, Common, Version, App, MiaoError }
