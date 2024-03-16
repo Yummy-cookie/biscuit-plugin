96d63dfe79daef1530d1b5cbf30b8199b54648d9
饼干 <2562248115@qq.com>
2024-03-16 10:16:10 +0800

    更新帮助

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-import { Data, Version } from './components/index.js'
-import Index from './apps/index2.js'
+import fs from "node:fs/promises";
+import chalk from 'chalk';
+import path from 'path';
 
-if (!global.segment)
-  global.segment = (await import('oicq')).segment
+logger.info(chalk.cyan('----------\(≧▽≦)/---------'));
+logger.info(chalk.yellow(`biscuit-plugin初始化~`));
+logger.info(chalk.yellow(`发送饼干帮助解锁功能哦~`));
+logger.info(chalk.magenta('-------------------------'));
 
-if (!segment.button)
-  segment.button = () => ""
+async function loadApps() {
+  try {
+    const appFiles = await fs.readdir('./plugins/biscuit-plugin/apps');
+    const appPromises = appFiles
+      .filter((file) => file.endsWith('.js'))
+      .map((file) => import(`./apps/${file}`));
 
-export * from './apps/index.js'
+    const appResults = await Promise.all(appPromises);
 
-if (Bot?.logger?.info) {
-  Bot.logger.info('---------^_^---------')
-  Bot.logger.info(`饼干插件${Version.version}初始化~`)
-} else {
-  console.log(`饼干插件${Version.version}初始化~`)
+    const apps = {};
+    for (let i = 0; i < appResults.length; i++) {
+      const name = appFiles[i].replace('.js', '');
+      apps[name] = appResults[i][Object.keys(appResults[i])[0]];
+    }
+
+    return apps;
+  } catch (error) {
+    logger.error(`载入插件错误：${logger.red(name)}`);
+    logger.error(error);
+    return {};
+  }
 }
 
-setTimeout(Index.init, 1000)
+export const apps = await loadApps();
