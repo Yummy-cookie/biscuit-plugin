2df3ad8dcef55503c19374885d078a747b5df2f1
饼干 <2562248115@qq.com>
2024-09-08 20:22:42 +0800

    细节优化

--- apps/ping工具.js
+++ apps/ping工具.js
@@ -22,26 +22,26 @@ export class PingPlugin extends plugin {
         if (!input) {
             return await this.e.reply('请输入要 ping 的域名或 IP 地址。');
         }
-
+    
         const url = `https://api.gumengya.com/Api/Ping?format=json&ip=${encodeURIComponent(input)}&type=ipv4`;
         
         try {
             const response = await fetch(url);
             const result = await response.json();
-
+    
             if (result.code === 200) {
                 const { data } = result;
                 const reply = `
-                    饼干Ping 结果:
-                    IP: ${data.ip}
-                    节点: ${data.node}
-                    主机: ${data.host}
-                    PING 信息: ${data.domain_ip}
-                    最小延迟: ${data.ping_min}
-                    平均延迟: ${data.ping_avg}
-                    最大延迟: ${data.ping_max}
-                    位置: ${data.location}
-                `;
+    饼干Ping 结果:
+    IP: ${data.ip}
+    节点: ${data.node}
+    主机: ${data.host}
+    PING 信息: ${data.domain_ip}
+    最小延迟: ${data.ping_min}
+    平均延迟: ${data.ping_avg}
+    最大延迟: ${data.ping_max}
+    位置: ${data.location}
+    `;
                 await this.e.reply(reply);
             } else {
                 await this.e.reply(`错误: ${result.msg}`);
@@ -51,4 +51,4 @@ export class PingPlugin extends plugin {
             await this.e.reply('请求失败，请稍后再试。');
         }
     }
-}
+    
