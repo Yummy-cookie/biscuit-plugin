4b30403955b01f05752c34463111ebec6bb3dc13
饼干 <2562248115@qq.com>
2024-09-08 20:27:23 +0800

    细节优化

--- apps/ping工具.js
+++ apps/ping工具.js
@@ -32,7 +32,7 @@ export class PingPlugin extends plugin {
             if (result.code === 200) {
                 const { data } = result;
                 const reply = `
-**Ping 结果:**
+饼干Ping 结果:
 IP: ${data.ip}
 节点: ${data.node}
 主机: ${data.host}
