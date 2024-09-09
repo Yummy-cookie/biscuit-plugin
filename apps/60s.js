776332a49c49361aff4172bbfb354c51cfb8076d
饼干 <2562248115@qq.com>
2024-09-10 00:02:55 +0800

    细节优化

--- apps/60s.js
+++ apps/60s.js
@@ -1,38 +1,24 @@
-import fetch from 'node-fetch';
+import plugin from '../../../lib/plugins/plugin.js';
 
-export class DailyNewsPlugin extends plugin {
-    constructor() {
-        super({
-            name: '每日早报插件',
-            dsc: '发送每日早报图片',
-            event: 'message',
-            priority: 2000,
-            rule: [
-                {
-                    reg: '^#?看60s$',
-                    fnc: 'sendNewsImage'
-                }
-            ]
-        });
-    }
+export class example extends plugin {
+	constructor() {
+		super({
+			name: 'biscuit插件集',
+			dsc: 'biscuit插件集',
+			event: 'message',
+			priority: 5000,
+			rule: [
+				{
+					reg: '^#?看60s.*$',
+					fnc: 'dailynews'
+				}
+			]
+		});
+	}
 
-    async sendNewsImage(e) {
-        try {
-            const imageUrl = 'https://api.ahfi.cn/api/MorningNews'; 
-            const imageBuffer = await this.getImageMessage(imageUrl);
-            await this.reply(imageBuffer); // 直接发送图片 Buffer
-        } catch (error) {
-            console.error('[每日早报插件][sendNewsImage] 发送早报时发生错误:', error);
-            await this.reply('发送早报失败，请稍后再试。');
-        }
-    }
+	async dailynews(e) {
+		this.e.reply(segment.image('https:///api/MorningNews'));
+		return true;
+	}
 
-    async getImageMessage(imageUrl) {
-        const response = await fetch(imageUrl);
-        if (!response.ok) {
-            throw new Error('无法获取早报图片');
-        }
-
-        return await response.buffer(); // 直接返回图片的 Buffer
-    }
 }
